e14787bb545cf1e20a38593f825fc1b35709619c
gg8249-code <gg8249@srmist.edu.in>
2026-02-26 15:52:17 +0530

    UC1: Add Welcome message module

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,13 @@
+// UC1 - Palindrome Checker Application (Basic Version)
+
+public class UseCase1PalindromeCheckerApp {
+
+    // Main method - Entry point of Java program
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Palindrome checker");
+        System.out.println("version 1.0");
+        System.out.println("System initialized successfully");
+
+    }
+}
